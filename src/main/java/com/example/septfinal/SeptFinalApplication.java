34061a7d785dbbee9996aefa1b0bcae125849322
Jed Opie <jed.opie@hotmail.com>
2022-09-09 13:34:17 +1000

    Initial commit

---
+++ src/main/java/com/example/septfinal/SeptFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.septfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeptFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeptFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/septfinal/SeptFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.septfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeptFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
